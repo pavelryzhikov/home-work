bf3635134322174b3922e3751939059ce1a7398c
Ryzhikov Pavel <pavelryzhikov@gmail.com>
2021-11-19 00:31:48 +0300

    смержил замечания урока 3

--- src/main/java/com/github/pavelryzhikov/Main.java
+++ src/main/java/com/github/pavelryzhikov/Main.java
@@ -1,6 +1,6 @@
 package com.github.pavelryzhikov;
 
-import com.github.pavelryzhikov.service.Account;
+import com.github.pavelryzhikov.dto.Account;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
