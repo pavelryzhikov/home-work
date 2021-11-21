c844b0145b653f0b574bc0fa33d75210b30e1a15
Ryzhikov Pavel <pavelryzhikov@gmail.com>
2021-11-21 14:27:59 +0300

    добавил Javadoc

--- src/main/java/com/github/pavelryzhikov/atm/cassettes/Banknote.java
+++ src/main/java/com/github/pavelryzhikov/atm/cassettes/Banknote.java
@@ -1,4 +1,8 @@
 package com.github.pavelryzhikov.atm.cassettes;
 
+/**
+ * класс реализующий сущность Банкнота
+ */
+
 public class Banknote {
 }
--- src/main/java/com/github/pavelryzhikov/atm/cassettes/Cassette.java
+++ src/main/java/com/github/pavelryzhikov/atm/cassettes/Cassette.java
@@ -7,12 +7,23 @@ import lombok.experimental.FieldDefaults;
 
 import java.util.ArrayList;
 
+/**
+ * класс реализующий сущность Кассета банкомата
+ *
+ * @param <T>
+ */
+
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class Cassette<T extends Banknote> {
 
     @NonNull ArrayList<T> cassetteStaff;
 
+    /**
+     * метод возвращает количество купюр в кассете
+     *
+     * @return
+     */
     public int getCountBanknotes() {
         return cassetteStaff.size();
     }
