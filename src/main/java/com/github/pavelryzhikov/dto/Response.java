6f28f9c03abbee49e903f661f46113a9af759a03
Ryzhikov Pavel <pavelryzhikov@gmail.com>
2021-12-18 00:11:28 +0300

    lesson-9: заменил конструкторы на аннотации

--- src/main/java/com/github/pavelryzhikov/dto/Response.java
+++ src/main/java/com/github/pavelryzhikov/dto/Response.java
@@ -1,17 +1,12 @@
 package com.github.pavelryzhikov.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Response {
-
     private String statusCode;
-
-    public Response() {
-    }
-
-    public Response(String statusCode) {
-        this.statusCode = statusCode;
-    }
-
 }
