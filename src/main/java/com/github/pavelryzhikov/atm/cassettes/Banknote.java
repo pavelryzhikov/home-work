2cd068087b616464421cca1598d4f205e7f1e78f
Ryzhikov Pavel <pavelryzhikov@gmail.com>
2022-02-09 20:57:55 +0300

    init lesson-12

--- src/main/java/com/github/pavelryzhikov/atm/cassettes/Banknote.java
+++
@@ -1,8 +0,0 @@
-package com.github.pavelryzhikov.atm.cassettes;
-
-/**
- * класс реализующий сущность Банкнота
- */
-
-public class Banknote {
-}
---
+++ src/main/java/com/sbrf/reboot/Main.java
@@ -0,0 +1,6 @@
+package com.sbrf.reboot;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
